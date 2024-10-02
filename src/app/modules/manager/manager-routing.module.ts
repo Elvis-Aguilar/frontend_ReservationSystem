101aa7f9dfc3c3d8454cb7383dba1df7468f443e
David15Barrera <david1315barrera@gmail.com>
2024-10-02 03:10:01 -0600

    add Routes

--- src/app/modules/manager/manager-routing.module.ts
+++ src/app/modules/manager/manager-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppointmentsAdminComponent } from './appointments-admin/appointments-admin.component';
+import { BillingComponent } from './billing/billing.component';
+import { BusinessComponent } from './business/business.component';
+import { ClientsComponent } from './clients/clients.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { SettingsComponent } from './settings/settings.component';
+
+const routes: Routes = [
+  {
+    path:'citas-admin', component: AppointmentsAdminComponent
+  },
+  {
+    path:'reportes', component: BillingComponent
+  },
+  {
+    path:'config-negocio', component: BusinessComponent
+  },
+  {
+    path:'edit-usuario', component: ClientsComponent
+  },
+  {
+    path:'empleados', component: EmployeesComponent
+  },
+  {
+    path:'configuracion', component: SettingsComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/manager/manager.module.ts
+++ src/app/modules/manager/manager.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ManagerRoutingModule } from './manager-routing.module';
-
+import { AppointmentsAdminComponent } from './appointments-admin/appointments-admin.component';
+import { BillingComponent } from './billing/billing.component';
+import { BusinessComponent } from './business/business.component';
+import { ClientsComponent } from './clients/clients.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ManagerRoutingModule
+    ManagerRoutingModule,
+    AppointmentsAdminComponent,
+    BillingComponent,
+    BusinessComponent,
+    ClientsComponent,
+    EmployeesComponent,
+    SettingsComponent
   ]
 })
 export class ManagerModule { }
