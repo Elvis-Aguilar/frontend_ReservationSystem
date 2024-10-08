b002541497a2eb6112a942a49e29bdcf4a1d0769
David15Barrera <david1315barrera@gmail.com>
2024-10-08 16:21:56 -0600

    added nav and setting

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isSidebarOpen = false;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
--- src/app/shared/layouts/client-layout/client-layout.component.ts
+++ src/app/shared/layouts/client-layout/client-layout.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-client-layout',
   standalone: true,
-  imports: [RouterModule, NavbarComponent],
+  imports: [RouterModule, NavbarComponent, CommonModule],
   templateUrl: './client-layout.component.html',
   styleUrl: './client-layout.component.scss'
 })
 export class ClientLayoutComponent {
+  isSidebarOpen = false;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
